b21d8acd9af74f5e2ce2b08cb083327cf280cc5c
KHAIHWAN <Khai_Chiao@outlook.co.th>
2025-09-15 15:02:35 +0700

    ➕ navigation menu dropdown switcher done

--- src/components/layout/front/header/DesktopMenu.tsx
+++ src/components/layout/front/header/DesktopMenu.tsx
@@ -1,5 +1,5 @@
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
-import { navigationAbout } from "./MockDataNavigtion";
+import { navigationAbout, navigationAcademic, navigationNews } from "./MockDataNavigtion";
 import { cn } from "@/lib/utils";
 import { useLocale } from 'next-intl';
 import { useTranslations } from 'next-intl';
@@ -9,8 +9,9 @@ export default function DesktopMenu({ className }: { className?: string }) {
     const translate = useTranslations('Navigation');
     
     return (
-        <NavigationMenu className={className}>
+        <NavigationMenu className={className} viewport={false}>
             <NavigationMenuList>
+                {/* About */}
                 <NavigationMenuItem>
                     <NavigationMenuTrigger className="text-sm font-medium leading-none [&_svg]:hidden">{translate('About')}</NavigationMenuTrigger>
                     <NavigationMenuContent>
@@ -46,6 +47,78 @@ export default function DesktopMenu({ className }: { className?: string }) {
                             ))}
                         </div>
                     </NavigationMenuContent>
+                </NavigationMenuItem>
+                {/* Academic */}
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger className="text-sm font-medium leading-none [&_svg]:hidden">{translate('Academic')}</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <div className="grid gap-3 p-4 w-100">
+                            {navigationAcademic.map((category) => (
+                                <div key={category.title} className="space-y-2">
+                                    <h4 className="text-sm font-medium leading-none">
+                                        {locale === 'th' ? category.titleTH : category.title}
+                                    </h4>
+                                    <hr className="my-2" />
+                                    <div className="grid gap-1">
+                                        {category.content.map((item) => (
+                                            <NavigationMenuLink
+                                                key={item.title}
+                                                href={item.url}
+                                                className={cn(
+                                                    "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                                )}
+                                            >
+                                                <div className="flex items-center gap-2">
+                                                    {item.icon && <item.icon className="h-4 w-4" />}
+                                                    <div className="text-sm leading-none">
+                                                        {locale === 'th' ? item.titleTH : item.title}
+                                                    </div>
+                                                </div>
+                                            </NavigationMenuLink>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                {/* News */}
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger className="text-sm font-medium leading-none [&_svg]:hidden">{translate('News')}</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <div className="grid gap-3 p-4 w-72">
+                            {navigationNews.map((category) => (
+                                <div key={category.title} className="space-y-2">
+                                    <h4 className="text-sm font-medium leading-none">
+                                        {locale === 'th' ? category.titleTH : category.title}
+                                    </h4>
+                                    <hr className="my-2" />
+                                    <div className="grid gap-1">
+                                        {category.content.map((item) => (
+                                            <NavigationMenuLink
+                                                key={item.title}
+                                                href={item.url}
+                                                className={cn(
+                                                    "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                                )}
+                                            >
+                                                <div className="flex items-center gap-2">
+                                                    {item.icon && <item.icon className="h-4 w-4" />}
+                                                    <div className="text-sm leading-none">
+                                                        {locale === 'th' ? item.titleTH : item.title}
+                                                    </div>
+                                                </div>
+                                            </NavigationMenuLink>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </NavigationMenuContent>    
+                </NavigationMenuItem>
+                {/* Contact */}
+                <NavigationMenuItem>
+                    <NavigationMenuLink href="/contact" className="text-sm font-medium leading-none [&_svg]:hidden">{translate('Contact')}</NavigationMenuLink>
                 </NavigationMenuItem>
             </NavigationMenuList>
         </NavigationMenu>
--- src/components/layout/front/header/MockDataNavigtion.tsx
+++ src/components/layout/front/header/MockDataNavigtion.tsx
@@ -1,6 +1,7 @@
 import { Atom,
 	Brain,
 	Briefcase,
+	Calendar,
 	CalendarDays,
 	CalendarHeart,
 	Compass,
@@ -102,6 +103,175 @@ export const navigationAbout: NavigationAbout[] = [
                 icon: Library,
                 description: "Another description.",
                 descriptionTH: "อื่นๆ"
+            }
+        ]
+    }
+]
+
+export interface dynamicContent {
+    title: string
+    titleTH: string
+    url: string
+    icon?: LucideIcon
+}
+
+export interface NavigationDynamic {
+    title: string
+    titleTH: string
+    content: dynamicContent[]
+}
+
+export const navigationAcademic: NavigationDynamic[] = [
+    {
+        title: "Master's Degree Program(s)",
+        titleTH: "หลักสูตรปริญญาโท",
+        content: [
+            {
+                title: "Master of Science Program in Biotechnology",
+                titleTH: "หลักสูตรวิทยาศาสตรมหาบัณฑิต สาขาวิชาเทคโนโลยีชีวภาพ",
+                url: "/academic/master-of-science-program-in-biotechnology",
+                icon: Dna
+            },
+            {
+                title: "Master of Science Program in Forensic Science",
+                titleTH: "หลักสูตรวิทยาศาสตรมหาบัณฑิต สาขาวิชานิติวิทยาศาสตร์",
+                url: "/academic/master-of-science-program-in-forensic-science",
+                icon: Fingerprint
+            },
+            {
+                title: "Master of Science Program in Sports Science",
+                titleTH: "หลักสูตรวิทยาศาสตรมหาบัณฑิต สาขาวิชาวิทยาศาสตร์การกีฬา",
+                url: "/academic/master-of-science-program-in-sports-science",
+                icon: Dumbbell
+            },
+            {
+                title: "Master of Science Program in Mental Healt",
+                titleTH: "หลักสูตรวิทยาศาสตรมหาบัณฑิต สาขาวิชาสุขภาพจิต",
+                url: "/academic/master-of-science-program-in-mental-healt",
+                icon: Brain
+            },
+            {
+                title: "Master of Arts Program in Contemporary Chinese Language Teaching",
+                titleTH: "หลักสูตรศิลปศาสตรมหาบัณฑิต สาขาวิชาการสอนภาษาจีนร่วมสมัย",
+                url: "/academic/master-of-arts-program-in-contemporary-chinese-language-teaching",
+                icon: Languages
+            },
+            {
+                title: "Master of Arts and Science Program in Integrated Science",
+                titleTH: "หลักสูตรศิลปศาสตรและวิทยาศาสตรมหาบัณฑิต สาขาวิชาบูรณาการศาสตร์",
+                url: "/academic/master-of-arts-and-science-program-in-integrated-science",
+                icon: Atom
+            }
+        ]
+    },
+    {
+        title: "Doctoral's Degree Program(s)",
+        titleTH: "หลักสูตรปริญญาเอก",
+        content: [
+            {
+                title: "Doctor of Philosophy Program in Biotechnology",
+                titleTH: "หลักสูตรปริญญาเอก สาขาวิชาเทคโนโลยีชีวภาพ",
+                url: "/academic/doctor-of-philosophy-program-in-biotechnology",
+                icon: Dna
+            },
+            {
+                title: "Doctor of Philosophy Program in Mental Healt",
+                titleTH: "หลักสูตรปรัชญาดุษฎีบัณฑิต สาขาวิชาสุขภาพจิต",
+                url: "/academic/doctor-of-philosophy-program-in-mental-healt",
+                icon: Brain
+            },
+            {
+                title: "Doctor of Philosophy Program in Integrated Science",
+                titleTH: "หลักสูตรปรัชญาดุษฎีบัณฑิต สาขาวิชาบูรณาการศาสตร์",
+                url: "/academic/doctor-of-philosophy-program-in-integrated-science",
+                icon: Atom
+            }
+        ]
+    }
+]
+
+export const navigationNews: NavigationDynamic[] = [
+    {
+        title: "Announcement",
+        titleTH: "ประกาศ",
+        content: [
+            {
+                title: "Announcement from CMU",
+                titleTH: "ประกาศจากมหาวิทยาลัย",
+                url: "/news/announcement-from-cmu",
+                icon: Megaphone
+            },
+            {
+                title: "Policy from CMU",
+                titleTH: "นโยบายจากมหาวิยาลัย",
+                url: "news/policy-from-cmu",
+                icon: ScrollText
+            }
+        ]
+    },
+    {
+        title: "Events",
+        titleTH: "กิจกรรม",
+        content: [
+            {
+                title: "All Events",
+                titleTH: "กิจกรรมทั้งหมด",
+                url: "/events/all-events",
+                icon: CalendarHeart
+            }
+        ]
+    },
+    {
+        title: "News",
+        titleTH: "ข่าวสาร",
+        content: [
+            {
+                title: "All News",
+                titleTH: "ข่าวสารทั้งหมด",
+                url: "/news/all-news",
+                icon: Newspaper
+            },
+            {
+                title: "General News",
+                titleTH: "ข่าวสารทั่วไป",
+                url: "/news/general-news",
+                icon: MessageSquare
+            },
+            {
+                title: "Activities News",
+                titleTH: "ข่าวสารกิจกรรม",
+                url: "/news/activities-news",
+                icon: CalendarDays
+            },
+            {
+                title: "Educational Service News",
+                titleTH: "ข่าวบริการการศึกษาและทุนการศึกษา",
+                url: "/news/educational-service-news",
+                icon: GraduationCap
+            },
+            {
+                title: "Exchange Programs and Scholarships",
+                titleTH: "โครงการแลกเปลี่ยนและทุนการศึกษาต่างประเทศ",
+                url: "/news/exchange-programs-and-scholarships",
+                icon: Globe2
+            },
+            {
+                title: "Awards and Research Achievements",
+                titleTH: "รางวัลและผลงานวิจัย",
+                url: "/news/awards-and-research-achievements",
+                icon: Trophy
+            },
+            {
+                title: "Job Application News",
+                titleTH: "ข่าวสมัครงาน",
+                url: "/news/job-application-news",
+                icon: Briefcase
+            },
+            {
+                title: "Procurement Announcements",
+                titleTH: "ข่าวประกวดราคา",
+                url: "/news/procurement-announcements",
+                icon: FileText
             }
         ]
     }
