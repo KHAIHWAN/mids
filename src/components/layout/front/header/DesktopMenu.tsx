7908b26eaac9a90d942b6005468e915cdcda9d37
KHAIHWAN <Khai_Chiao@outlook.co.th>
2025-09-15 11:38:58 +0700

    📌 example switcher language in mock data

--- src/components/layout/front/header/DesktopMenu.tsx
+++ src/components/layout/front/header/DesktopMenu.tsx
@@ -1,18 +1,25 @@
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
 import { navigationAbout } from "./MockDataNavigtion";
 import { cn } from "@/lib/utils";
+import { useLocale } from 'next-intl';
+import { useTranslations } from 'next-intl';
 
 export default function DesktopMenu({ className }: { className?: string }) {
+    const locale = useLocale();
+    const translate = useTranslations('Navigation');
+    
     return (
         <NavigationMenu className={className}>
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger className="text-sm font-medium leading-none [&_svg]:hidden">About</NavigationMenuTrigger>
+                    <NavigationMenuTrigger className="text-sm font-medium leading-none [&_svg]:hidden">{translate('About')}</NavigationMenuTrigger>
                     <NavigationMenuContent>
                         <div className="grid gap-3 p-4 w-72">
                             {navigationAbout.map((category) => (
                                 <div key={category.title} className="space-y-2">
-                                    <h4 className="text-sm font-medium leading-none">{category.title}</h4>
+                                    <h4 className="text-sm font-medium leading-none">
+                                        {locale === 'th' ? category.titleTH : category.title}
+                                    </h4>
                                     <hr className="my-2" />
                                     <div className="grid gap-1">
                                         {category.content.map((item) => (
@@ -25,10 +32,12 @@ export default function DesktopMenu({ className }: { className?: string }) {
                                             >
                                                 <div className="flex items-center gap-2">
                                                     {item.icon && <item.icon className="h-4 w-4" />}
-                                                    <div className="text-sm font-medium leading-none">{item.title}</div>
+                                                    <div className="text-sm font-medium leading-none">
+                                                        {locale === 'th' ? item.titleTH : item.title}
+                                                    </div>
                                                 </div>
                                                 <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                                                    {item.description}
+                                                    {locale === 'th' ? item.descriptionTH : item.description}
                                                 </p>
                                             </NavigationMenuLink>
                                         ))}
